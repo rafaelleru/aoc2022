093a5ef40171ec90d16b94f5276655cb0320bfba
Rafa Leyva <rafael.leyva@constellaintelligence.com>
2023-01-05 12:35:40 +0100

    Solved part day 1 part 1

---
+++ day1/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let content = include_str!("input.txt");
+    let calories : Vec<&str> = content.trim().split("\n\n").collect();
+    let mut sums : Vec<i32> = Vec::new();
+
+    for c in calories {
+        let partials : Vec<&str> = c.split("\n").collect();
+        let mut sum = 0;
+        for p in partials {
+            println!(" \" {} \" ", p);
+            if p != "\n" {
+                sum += p.parse::<i32>().unwrap()
+            }
+        }
+
+        sums.push(sum);
+    }
+
+    println!("{}", sums.iter().max().unwrap());
+
+}
