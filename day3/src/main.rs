1847bf1b9b129e994d900fc01a634595c7dd7ba5
Rafa Leyva <rafael.leyva@constellaintelligence.com>
2023-01-08 21:04:12 +0100

    Day 3 removed hashmap with scores

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,60 +1,17 @@
-use std::collections::HashMap;
+fn char_to_point (c: char) -> u32 {
+    let points : u32;
+    if c > 'Z' {
+        // 'a' is ASCII code 96 but priority 1, so we subtract 96
+        points = (c as u32) - 96;
+    } else {
+        // 'A' is ASCII code 65 but priority 27, so we subtract 38
+        points = (c as u32) - 38;
+    }
+
+    return points
+}
 
 fn main() {
-    let priorities : HashMap<char, i32> = vec![
-        ('a', 1),
-        ('b', 2),
-        ('c', 3),
-        ('d', 4),
-        ('e', 5),
-        ('f', 6),
-        ('g', 7),
-        ('h', 8),
-        ('i', 9),
-        ('j', 10),
-        ('k', 11),
-        ('l', 12),
-        ('m', 13),
-        ('n', 14),
-        ('o', 15),
-        ('p', 16),
-        ('q', 17),
-        ('r', 18),
-        ('s', 19),
-        ('t', 20),
-        ('u', 21),
-        ('v', 22),
-        ('w', 23),
-        ('x', 24),
-        ('y', 25),
-        ('z', 26),
-        ('A', 27),
-        ('B', 28),
-        ('C', 29),
-        ('D', 30),
-        ('E', 31),
-        ('F', 32),
-        ('G', 33),
-        ('H', 34),
-        ('I', 35),
-        ('J', 36),
-        ('K', 37),
-        ('L', 38),
-        ('M', 39),
-        ('N', 40),
-        ('O', 41),
-        ('P', 42),
-        ('Q', 43),
-        ('R', 44),
-        ('S', 45),
-        ('T', 46),
-        ('U', 47),
-        ('V', 48),
-        ('W', 49),
-        ('X', 50),
-        ('Y', 51),
-        ('Z', 52),
-    ].into_iter().collect();
 
     let input = include_str!("../input.txt");
     let rucksacks : Vec<&str> = input.trim().split("\n").collect::<Vec<&str>>();  // Convert the spit struct to a Vector of string
@@ -75,8 +32,8 @@ fn main() {
 
     }
 
-    let points : i32 = duplicated.iter()
-        .map(|c| priorities[&c])
+    let points : u32 = duplicated.iter()
+        .map(|c| char_to_point(*c))
         .sum();
 
     println!("{}", points);
@@ -95,8 +52,8 @@ fn main() {
         i += 3
     }
 
-    let points : i32 = badges.iter()
-        .map(|c| priorities[&c])
+    let points : u32 = badges.iter()
+        .map(|c| char_to_point(*c))
         .sum();
 
     println!("{}", points);
