8b915ffcea8b00dad4090d948dcf51b7b6691e79
Rafa Leyva <rafael.leyva@constellaintelligence.com>
2023-01-09 08:49:33 +0100

    Update directories name to be shorted

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -3,6 +3,17 @@ fn main() {
     let calories : Vec<&str> = content.trim().split("\n\n").collect();
     let mut sums : Vec<i32> = Vec::new();
 
+    // TODO: no funciona en un solo bloque
+    //let mut sums : Vec<i32> = content.trim().split("\n\n")
+        //.collect::<Vec<&str>>()
+        //.iter()
+        //.map(|c| c.split("\n").collect())
+        //.collect::<Vec<&str>>()
+        //.iter()
+        //.map(|p| p.parse::<i32>().unwrap().sum::<i32>())
+        //.collect();
+
+
     for c in calories {
         let partials : Vec<&str> = c.split("\n").collect();
         sums.push(partials.iter().map(|p| p.parse::<i32>().unwrap()).sum::<i32>());
