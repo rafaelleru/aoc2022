17d1d12b06f6309e79b2b94b2d73ab1a0ee8d61c
Rafa Leyva <rafael.leyva@constellaintelligence.com>
2023-01-23 14:43:23 +0100

    Day 6, create function to avoid duplicate code

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,8 +1,8 @@
-fn main() {
-    let input : Vec<char> = include_str!("../input.txt").trim().chars().collect();
+fn search_message(input: &Vec<char>, window_size: usize) -> usize {
+    let mut window_index = 0;
 
-    for (i, window) in input.windows(4).enumerate() {
-        // Use vector and insert as show by ThePrimeagean in the Yt video explaining this problem.
+    for (i, window) in input.windows(window_size).enumerate() {
+        // Use vector and insert as show by ThePrimeagean in the YouTube video explaining this problem.
         // for this size of vector insert and check linearly than using a data type such as a set.
         let mut idx : Vec<char> = Vec::new();
 
@@ -14,29 +14,20 @@ fn main() {
             }
         }
 
-        if idx.len() == 4 {
-            println!("Solution for Part 1 is: {}", i+4);
+
+        if idx.len() == window_size {
+            window_index = i + window_size;
             break;
         }
     }
 
+    return window_index;
 
-    // Part 2
-    for (i, window) in input.windows(14).enumerate() {
-        let mut idx : Vec<char> = Vec::new();
+}
 
-        for c in window {
-            if idx.contains(c) {
-                break;
-            } else {
-                idx.push(*c);
-            }
-        }
+fn main() {
+    let input : Vec<char> = include_str!("../input.txt").trim().chars().collect();
 
-        if idx.len() == 14 {
-            println!("Solution for part 2 is: {}" , i+14);
-            break;
-        }
-    }
-
+    println!("Solution for part 1 is: {}", search_message(&input, 4));
+    println!("Solution for part 2 is: {}", search_message(&input, 14));
 }
