3bc87b9a454ea538f428c586eb277cb9ec0e2a8a
Rafa Leyva <rafael.leyva@constellaintelligence.com>
2023-01-26 18:32:42 +0100

    Solve day 8 part 2, not the best code

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -59,6 +59,115 @@ fn visible_from_right(map: &Vec<Vec<u32>>, row: usize, column: usize) -> bool {
 
 }
 
+fn get_viewing_distance_top(map: &Vec<Vec<u32>>, row: usize, column: usize) -> u32 {
+    let tree_size = map[row][column];
+    let mut viewing_distance_top = 0;
+    let mut i = 0;
+
+    if row == 0 {
+        return 0;
+    }
+
+    if row > 0 {
+        i = row - 1;
+    }
+
+    while i > 0 {
+        if map[i][column] < tree_size {
+            viewing_distance_top += 1;
+        } else {
+            viewing_distance_top += 1;
+            break;
+        }
+
+        i -= 1;
+    }
+
+    if i == 0 && map[0][column] <= tree_size {
+        viewing_distance_top += 1;
+    }
+
+    return viewing_distance_top;
+}
+
+fn get_viewing_distance_right(map: &Vec<Vec<u32>>, row: usize, column: usize) -> u32 {
+    let tree_size = map[row][column];
+    let mut viewing_distance_top = 0;
+
+    if column == map.first().unwrap().len() -1 {
+        return 0;
+    }
+
+    let mut i = column + 1;
+
+
+    while i < map.first().unwrap().len() {
+        if map[row][i] < tree_size {
+            viewing_distance_top += 1;
+        } else {
+            viewing_distance_top += 1;
+            break;
+        }
+
+        i += 1;
+    }
+
+    return viewing_distance_top;
+}
+
+fn get_viewing_distance_left(map: &Vec<Vec<u32>>, row: usize, column: usize) -> u32 {
+    let tree_size = map[row][column];
+    let mut viewing_distance_top = 0;
+    let mut i = 0;
+    if column == 0 {
+        return 0;
+    }
+
+    if column > 0 {
+        i = column - 1;
+    }
+
+    while i > 0 {
+        if map[row][i] < tree_size {
+            viewing_distance_top += 1;
+        } else {
+            viewing_distance_top += 1;
+            break;
+        }
+
+        i -= 1;
+    }
+
+    if i == 0 && map[row][0] < tree_size {
+        viewing_distance_top += 1;
+    }
+
+    return viewing_distance_top;
+}
+
+fn get_viewing_distance_bottom(map: &Vec<Vec<u32>>, row: usize, column: usize) -> u32 {
+    let tree_size = map[row][column];
+    let mut viewing_distance_top = 0;
+    if row == map.len() - 1 {
+        return 0;
+    }
+
+    let mut i = row + 1;
+
+    while i < map.len() {
+        if map[i][column] < tree_size {
+            viewing_distance_top += 1;
+        } else {
+            viewing_distance_top += 1;
+            break;
+        }
+
+        i += 1;
+    }
+
+    return viewing_distance_top;
+}
+
 fn main() {
     let map = include_str!("../input.txt")
         .trim()
@@ -80,6 +189,31 @@ fn main() {
             }
         }
     }
-
     println!("{} visibles", nvisible);
+
+    // Part 2
+    let mut best_scenic_score = 0;
+    for i in 0..map.len() {
+        for j in 0..map.first().unwrap().len() {
+            let viewing_distance_top = get_viewing_distance_top(&map, i, j);
+            let viewing_distance_left = get_viewing_distance_left(&map, i, j);
+            let viewing_distance_right = get_viewing_distance_right(&map, i, j);
+            let viewing_distance_bottom = get_viewing_distance_bottom(&map, i, j);
+
+            let scenic_score = viewing_distance_top * viewing_distance_right * viewing_distance_left * viewing_distance_bottom;
+
+            println!("Cheking scenic score for {} is {}", map[i][j], scenic_score);
+            println!("top: {}", viewing_distance_top);
+            println!("bottom: {}", viewing_distance_bottom);
+            println!("left: {}", viewing_distance_left);
+            println!("right: {}", viewing_distance_right);
+
+            if scenic_score > best_scenic_score {
+                best_scenic_score = scenic_score;
+            }
+        }
+    }
+    println!("Best scenic score is: {}", best_scenic_score);
+
+
 }
